19c44223e1cf486a24703606b5a3bbfcf99b4aa3
Your Name <your.email@example.com>
2024-03-30 16:28:19 -0400

    add documentation to decryptFile function

--- decrypt.js
+++ decrypt.js
@@ -3,16 +3,32 @@ const readline = require('readline');
 const crypto = require('crypto');
 const algorithm = 'aes-256-ctr';
 
+
+/**
+ *  * ----------------------
+ *  * function decryptFile
+ *  * ----------------------
+ * This function decrypts an aes-256-ctr encrypted file using the specified password and saves decrypted content to a new file.
+ *
+ * @param {string} file - The path to the encrypted file.
+ * @param {string} decryptedFile - The path where the decrypted file will be saved.
+ * @param {string} password - The decryption password.
+*/
+
 function decryptFile(file, decryptedFile, password) {
+// read file, assign it the name encryptedData
   const encryptedData = fs.readFileSync(file);
+// Extract the Initialization Vector (IV) from the encrypted data. Initialization vector is a "nonce". It is a random piece of data to add randomness to the encryption.
   const IV = encryptedData.slice(0, 16);
+  // Extract the encrypted data starting from index 16, right after the Initialization Vector (IV).
   const data = encryptedData.slice(16);
+  //Generates a hashing for the password and using this hashed password, along with the provided algorithm and IV, to create a decryption decipher.
   let decipher = crypto.createDecipheriv(algorithm, crypto.createHash('sha256').update(password).digest(), IV);
+  // Decrypt the 'data' using the 'update' method and finalize the decryption process using the 'final' method. The results are concatenated into a single Buffer to produce the completed decrypted data.
   let decrypted = Buffer.concat([decipher.update(data), decipher.final()]);
+  //write the decrypted data to the new file
   fs.writeFileSync(decryptedFile, decrypted);
 }
-
-let sourceFile, destinationFile;
 
 // Prepare readline interface
 const readlineInterface = readline.createInterface({
@@ -28,6 +44,7 @@ readlineInterface._writeToOutput = function _writeToOutput(stringToWrite) {
 };
 
 readlineInterface.question('Please provide the filepath to the file you want to decrypt: ', filePath => {
+  let sourceFile, destinationFile;
   // Set the source file and destination filepath
   sourceFile = filePath;
   // We need to replace "Encrypted" with "Decrypted" in the file name
